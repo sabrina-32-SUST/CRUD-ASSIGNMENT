4cd15fd98294895aff6b21aead5e4ddeca2b0831
Sabrina <sanutaayvi@gmail.com>
2021-09-20 13:26:04 +0600

    about added

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Home from "./component/pages/Home";
-
+import About from './component/pages/About';
+import Contact from './component/pages/Contact';
 function App() {
   return (
     <div className="App">
-     <h1>Hello world</h1>
-     <home/>
+
+     <Home />
+     <About />
+     <Contact />
+
+
     </div>
   );
 }
