0cf46861936f363aef0fed414c415c309895ef19
Sabrina <sanutaayvi@gmail.com>
2021-09-20 15:06:01 +0600

    Navbar done

--- assignment/src/component/layout/navbar.js
+++ assignment/src/component/layout/navbar.js
@@ -1,36 +1,47 @@
 import React from "react";
-import {Link} from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
-    return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <Link className="navbar-brand" href="/">
+          Recat User
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
- <div class="container">
-                <a class="navbar-brand" href="#">React User</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse"  >
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="\home">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link  " aria-current="page" href="\about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link  " aria-current="page" href="\contact">Contact</a>
-                        </li>
-                       
-                    </ul>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
 
-                </div>
-            </div>
-       
-          
-           
-            </nav>
-    )
-}
- 
-export default   Navbar;
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
