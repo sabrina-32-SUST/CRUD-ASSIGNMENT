6011a35a96b84a283bd86029b4b5ec6ba5973321
Sabrina <sanutaayvi@gmail.com>
2021-09-20 17:06:51 +0600

    user edit   updated

--- assignment/src/component/users/User.js
+++ assignment/src/component/users/User.js
@@ -19,3 +19,23 @@ const User = () => {
     const res = await axios.get(`http://localhost:3003/users/${id}`);
     setUser(res.data);
   };
+
+  return (
+    <div className="container py-4">
+      <Link className="btn btn-primary" to="/">
+        back to Home
+      </Link>
+      <h1 className="display-4">User Id: {id}</h1>
+      <hr />
+      <ul className="list-group w-50">
+        <li className="list-group-item">name: {user.name}</li>
+        <li className="list-group-item">user name: {user.username}</li>
+        <li className="list-group-item">email: {user.email}</li>
+        <li className="list-group-item">phone: {user.phone}</li>
+        <li className="list-group-item">website: {user.website}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default User;
