608b76315c6768b9967d4c9d67ae5f64f95be00b
Sabrina <sanutaayvi@gmail.com>
2021-09-20 13:50:48 +0600

    navbar updated

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -3,10 +3,12 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Home from "./component/pages/Home";
 import About from './component/pages/About';
 import Contact from './component/pages/Contact';
+import Navbar from './component/layout/navbar';
 function App() {
   return (
     <div className="App">
 
+<Navbar />
      <Home />
      <About />
      <Contact />
