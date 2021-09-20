b224cdea9f87fa7dfc3a4a3657b3dd289af9b8a8
Sabrina <sanutaayvi@gmail.com>
2021-09-20 17:03:16 +0600

    user edit  page

---
+++ assignment/src/component/users/EditUser.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory, useParams } from "react-router-dom";
+
+const EditUser = () => {
+  let history = useHistory();
+  const { id } = useParams();
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    website: ""
+  });
