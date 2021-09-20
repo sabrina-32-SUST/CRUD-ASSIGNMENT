3f85238ae17cb5da38b2fe81c312e27a5a079bc7
Sabrina <sanutaayvi@gmail.com>
2021-09-20 13:22:08 +0600

    home added

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./component/pages/Home";
+
 function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
+     <home/>
     </div>
   );
 }
