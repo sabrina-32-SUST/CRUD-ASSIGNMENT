71b9f26407683f8ed4047dbd1299edd951c69f7d
Sabrina <sanutaayvi@gmail.com>
2021-09-20 12:59:51 +0600

    first commit

---
+++ assignment/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
